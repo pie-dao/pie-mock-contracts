82616c5d5506f02b697ceb596a5723b0b6f91711
Mick de Graaf <mick@mickdegraaf.nl>
2020-03-08 14:51:05 +0100

    Initial commit

---
+++ contracts/MockToken.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.6.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockToken is ERC20Detailed, ERC20 {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20Detailed(_name, _symbol, _decimals) public {
+        // NOTHING
+    }
+
+    function mint(address _to, uint256 _amount) external {
+        _mint(_to, _amount);
+    }
+
+    function burn(address _from, uint256 _amount) external {
+        _burn(_from, _amount);
+    }
+}
